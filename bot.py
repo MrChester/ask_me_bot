cd524586f9730e81d9bd6d250cb0e8fe41f43af7
Yauheni Ramankou <iromankov2010@gmail.com>
2019-10-18 19:37:34 +0300

    refactor: change code formatting according PeP8

--- bot.py
+++ bot.py
@@ -28,6 +28,8 @@ class TelegramBot:
         return None
 
     def send_message(self, chat_id, text):
-        url = self.url + self.bot_token
-        + "/" + f"sendmessage?chat_id={chat_id}&text={text}"
+        url = self.url \
+            + self.bot_token \
+            + "/" \
+            + f"sendmessage?chat_id={chat_id}&text={text}"
         return requests.get(url)
